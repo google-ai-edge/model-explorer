c6ed47b7186e3fe0b99ec1dfe99c39c9b722e6fc
Eric Yang <yang.yijie97@gmail.com>
2024-10-10 14:59:01 -0700

    Fix path error

--- ci/playwright_test.py
+++ ci/playwright_test.py
@@ -17,10 +17,13 @@ import re
 import time
 from playwright.sync_api import Page, expect
 from PIL import Image, ImageChops
+from pathlib import Path
 
-LOCAL_SERVER = "http://127.0.0.1:8081//"
-TEST_FILES_DIR = "src/server/test/"
-EXPECTED_SCREENSHOT_DIR = "src/server/screenshots/"
+LOCAL_SERVER = "http://127.0.0.1:8080//"
+ROOT_DIR = Path(__file__).parent.parent
+TEST_FILES_DIR = ROOT_DIR / "src/server/test"
+TMP_SCREENSHOT_DIR = ROOT_DIR / ".tmp"
+EXPECTED_SCREENSHOT_DIR = ROOT_DIR / "src/server/screenshots"
 
 
 def matched_images(actual_image_path, expected_image_path, threshold: int = 10):
@@ -36,67 +39,67 @@ def matched_images(actual_image_path, expected_image_path, threshold: int = 10):
   return False
 
 
-def test_homepage(page: Page, tmp_path):
+def test_homepage(page: Page):
   page.goto(LOCAL_SERVER)
   expect(page).to_have_title(re.compile("Model Explorer"))
-  actual_image_path = tmp_path / "homepage.png"
+  actual_image_path = TMP_SCREENSHOT_DIR / "homepage.png"
   page.screenshot(path=actual_image_path)
-  expected_image_path = EXPECTED_SCREENSHOT_DIR + "homepage.png"
+  expected_image_path = EXPECTED_SCREENSHOT_DIR / "homepage.png"
   assert matched_images(actual_image_path, expected_image_path)
 
 
-def test_litert_direct_adapter(page: Page, tmp_path):
+def test_litert_direct_adapter(page: Page):
   page.goto(LOCAL_SERVER)
   page.get_by_placeholder("Absolute file paths (").fill(
-      TEST_FILES_DIR + "fully_connected.tflite"
+      TEST_FILES_DIR / "fully_connected.tflite"
   )
   page.get_by_role("button", name="Add").click()
   page.get_by_text("arrow_drop_down").click()
   page.get_by_text("TFLite adapter (Flatbuffer)").click()
   page.get_by_role("button", name="View selected models").click()
   page.locator("canvas").first.click(position={"x": 469, "y": 340})
-  actual_image_path = tmp_path / "litert_direct.png"
+  actual_image_path = TMP_SCREENSHOT_DIR / "litert_direct.png"
   page.screenshot(path=actual_image_path)
-  expected_image_path = EXPECTED_SCREENSHOT_DIR + "litert_direct.png"
+  expected_image_path = EXPECTED_SCREENSHOT_DIR / "litert_direct.png"
   assert matched_images(actual_image_path, expected_image_path)
 
 
-def test_litert_mlir_adapter(page: Page, tmp_path):
+def test_litert_mlir_adapter(page: Page):
   page.goto(LOCAL_SERVER)
   page.get_by_placeholder("Absolute file paths (").fill(
-      TEST_FILES_DIR + "fully_connected.tflite"
+      TEST_FILES_DIR / "fully_connected.tflite"
   )
   page.get_by_role("button", name="Add").click()
   page.get_by_text("arrow_drop_down").click()
   page.get_by_text("TFLite adapter (MLIR)").click()
   page.get_by_role("button", name="View selected models").click()
   page.locator("canvas").first.click(position={"x": 514, "y": 332})
-  actual_image_path = tmp_path / "litert_mlir.png"
+  actual_image_path = TMP_SCREENSHOT_DIR / "litert_mlir.png"
   page.screenshot(path=actual_image_path)
-  expected_image_path = EXPECTED_SCREENSHOT_DIR + "litert_mlir.png"
+  expected_image_path = EXPECTED_SCREENSHOT_DIR / "litert_mlir.png"
   assert matched_images(actual_image_path, expected_image_path)
 
 
-def test_tf_mlir_adapter(page: Page, tmp_path):
+def test_tf_mlir_adapter(page: Page):
   page.goto(LOCAL_SERVER)
   page.get_by_placeholder("Absolute file paths (").fill(
-      TEST_FILES_DIR + "simple_add/saved_model.pb"
+      TEST_FILES_DIR / "simple_add/saved_model.pb"
   )
   page.get_by_role("button", name="Add").click()
   page.get_by_text("arrow_drop_down").click()
   page.get_by_text("TF adapter (MLIR) Default").click()
   page.get_by_role("button", name="View selected models").click()
   page.locator("canvas").first.click(position={"x": 444, "y": 281})
-  actual_image_path = tmp_path / "tf_mlir.png"
+  actual_image_path = TMP_SCREENSHOT_DIR / "tf_mlir.png"
   page.screenshot(path=actual_image_path)
-  expected_image_path = EXPECTED_SCREENSHOT_DIR + "tf_mlir.png"
+  expected_image_path = EXPECTED_SCREENSHOT_DIR / "tf_mlir.png"
   assert matched_images(actual_image_path, expected_image_path)
 
 
-def test_tf_direct_adapter(page: Page, tmp_path):
+def test_tf_direct_adapter(page: Page):
   page.goto(LOCAL_SERVER)
   page.get_by_placeholder("Absolute file paths (").fill(
-      TEST_FILES_DIR + "simple_add/saved_model.pb"
+      TEST_FILES_DIR / "simple_add/saved_model.pb"
   )
   page.get_by_role("button", name="Add").click()
   page.get_by_text("arrow_drop_down").click()
@@ -105,37 +108,37 @@ def test_tf_direct_adapter(page: Page, tmp_path):
   page.get_by_text("__inference__traced_save_36", exact=True).click()
   page.get_by_text("__inference_add_6").click()
   page.locator("canvas").first.click(position={"x": 723, "y": 278})
-  actual_image_path = tmp_path / "tf_direct.png"
+  actual_image_path = TMP_SCREENSHOT_DIR / "tf_direct.png"
   page.screenshot(path=actual_image_path)
-  expected_image_path = EXPECTED_SCREENSHOT_DIR + "tf_direct.png"
+  expected_image_path = EXPECTED_SCREENSHOT_DIR / "tf_direct.png"
   assert matched_images(actual_image_path, expected_image_path)
 
 
-def test_tf_graphdef_adapter(page: Page, tmp_path):
+def test_tf_graphdef_adapter(page: Page):
   page.goto(LOCAL_SERVER)
   page.get_by_placeholder("Absolute file paths (").fill(
-      TEST_FILES_DIR + "graphdef_foo.pbtxt"
+      TEST_FILES_DIR / "graphdef_foo.pbtxt"
   )
   page.get_by_role("button", name="Add").click()
   page.get_by_role("button", name="View selected models").click()
   page.locator("canvas").first.click(position={"x": 468, "y": 344})
-  actual_image_path = tmp_path / "graphdef.png"
+  actual_image_path = TMP_SCREENSHOT_DIR / "graphdef.png"
   page.screenshot(path=actual_image_path)
-  expected_image_path = EXPECTED_SCREENSHOT_DIR + "graphdef.png"
+  expected_image_path = EXPECTED_SCREENSHOT_DIR / "graphdef.png"
   assert matched_images(actual_image_path, expected_image_path)
 
 
-def test_shlo_mlir_adapter(page: Page, tmp_path):
+def test_shlo_mlir_adapter(page: Page):
   page.goto(LOCAL_SERVER)
   page.get_by_placeholder("Absolute file paths (").fill(
-      TEST_FILES_DIR + "stablehlo_sin.mlir"
+      TEST_FILES_DIR / "stablehlo_sin.mlir"
   )
   page.get_by_role("button", name="Add").click()
   page.get_by_role("button", name="View selected models").click()
   page.locator("canvas").first.dblclick(position={"x": 442, "y": 339})
   time.sleep(0.5)  # Delay for the animation
   page.locator("canvas").first.click(position={"x": 488, "y": 408})
-  actual_image_path = tmp_path / "shlo_mlir.png"
+  actual_image_path = TMP_SCREENSHOT_DIR / "shlo_mlir.png"
   page.screenshot(path=actual_image_path)
-  expected_image_path = EXPECTED_SCREENSHOT_DIR + "shlo_mlir.png"
+  expected_image_path = EXPECTED_SCREENSHOT_DIR / "shlo_mlir.png"
   assert matched_images(actual_image_path, expected_image_path)
