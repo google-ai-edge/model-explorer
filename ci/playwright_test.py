7af211c42859cf595b79099aa7d7036600b136b4
Eric Yang <yang.yijie97@gmail.com>
2024-10-09 23:13:35 -0700

    Add screenshots

--- ci/playwright_test.py
+++ ci/playwright_test.py
@@ -19,19 +19,19 @@ from playwright.sync_api import Page, expect
 from PIL import Image, ImageChops
 
 LOCAL_SERVER = "http://localhost:8080/"
-TEST_FILES_DIR = "/Users/yijieyang/tmp/test/"
-TEST_SCREENSHOT_DIR = "/Users/yijieyang/tmp/test/test_screenshots/"
-ACTUAL_SCREENSHOT_DIR = "/Users/yijieyang/tmp/screenshots/"
+TEST_FILES_DIR = "src/server/test/"
+TMP_SCREENSHOT_DIR = "src/server/test/tmp/"
+EXPECTED_SCREENSHOT_DIR = "src/server/screenshots/"
 
 
 def compare_images(image_suffix: str, threshold: int = 0):
-	test_image = Image.open(TEST_SCREENSHOT_DIR + image_suffix).convert("L")
-	actual_image = Image.open(ACTUAL_SCREENSHOT_DIR + image_suffix).convert("L")
+	tmp_image = Image.open(TMP_SCREENSHOT_DIR + image_suffix).convert("L")
+	expected_image = Image.open(EXPECTED_SCREENSHOT_DIR + image_suffix).convert("L")
 
-	if test_image.size != actual_image.size:
+	if tmp_image.size != expected_image.size:
 		return False
 
-	diff = ImageChops.difference(test_image, actual_image)
+	diff = ImageChops.difference(tmp_image, expected_image)
 	max_diff = diff.getextrema()[1]
 	if max_diff <= threshold:
 		return True
@@ -41,7 +41,7 @@ def compare_images(image_suffix: str, threshold: int = 0):
 def test_homepage(page: Page):
   page.goto(LOCAL_SERVER)
   expect(page).to_have_title(re.compile("Model Explorer"))
-  page.screenshot(path=TEST_SCREENSHOT_DIR + "homepage.png")
+  page.screenshot(path=TMP_SCREENSHOT_DIR + "homepage.png")
   assert compare_images("homepage.png")
 
 
@@ -53,7 +53,7 @@ def test_litert_direct_adapter(page: Page):
 	page.get_by_text("TFLite adapter (Flatbuffer)").click()
 	page.get_by_role("button", name="View selected models").click()
 	page.locator("canvas").first.click(position={"x":469,"y":340})
-	page.screenshot(path=TEST_SCREENSHOT_DIR + "litert_direct.png")
+	page.screenshot(path=TMP_SCREENSHOT_DIR + "litert_direct.png")
 	assert compare_images("litert_direct.png")
 
 
@@ -65,7 +65,7 @@ def test_litert_mlir_adapter(page: Page):
 	page.get_by_text("TFLite adapter (MLIR)").click()
 	page.get_by_role("button", name="View selected models").click()
 	page.locator("canvas").first.click(position={"x":514,"y":332})
-	page.screenshot(path=TEST_SCREENSHOT_DIR + "litert_mlir.png")
+	page.screenshot(path=TMP_SCREENSHOT_DIR + "litert_mlir.png")
 	assert compare_images("litert_mlir.png")
 
 
@@ -77,7 +77,7 @@ def test_tf_mlir_adapter(page: Page):
 	page.get_by_text("TF adapter (MLIR) Default").click()
 	page.get_by_role("button", name="View selected models").click()
 	page.locator("canvas").first.click(position={"x":444,"y":281})
-	page.screenshot(path=TEST_SCREENSHOT_DIR + "tf_mlir.png")
+	page.screenshot(path=TMP_SCREENSHOT_DIR + "tf_mlir.png")
 	assert compare_images("tf_mlir.png")
 
 
@@ -92,7 +92,7 @@ def test_tf_direct_adapter(page: Page):
 	page.get_by_text("__inference__traced_save_36", exact=True).click()
 	page.get_by_text("__inference_add_6").click()
 	page.locator("canvas").first.click(position={"x":723,"y":278})
-	page.screenshot(path=TEST_SCREENSHOT_DIR + "tf_direct.png")
+	page.screenshot(path=TMP_SCREENSHOT_DIR + "tf_direct.png")
 	assert compare_images("tf_direct.png")
 
 
@@ -102,7 +102,7 @@ def test_tf_graphdef_adapter(page: Page):
 	page.get_by_role("button", name="Add").click()
 	page.get_by_role("button", name="View selected models").click()
 	page.locator("canvas").first.click(position={"x":468,"y":344})
-	page.screenshot(path=TEST_SCREENSHOT_DIR +"graphdef.png")
+	page.screenshot(path=TMP_SCREENSHOT_DIR +"graphdef.png")
 	assert compare_images("graphdef.png")
 
 
@@ -114,5 +114,5 @@ def test_shlo_mlir_adapter(page: Page):
 	page.locator("canvas").first.dblclick(position={"x":442,"y":339})
 	time.sleep(0.5)  # Delay for the animation
 	page.locator("canvas").first.click(position={"x":488,"y":408})
-	page.screenshot(path=TEST_SCREENSHOT_DIR + "shlo_mlir.png")
+	page.screenshot(path=TMP_SCREENSHOT_DIR + "shlo_mlir.png")
 	assert compare_images("shlo_mlir.png")
